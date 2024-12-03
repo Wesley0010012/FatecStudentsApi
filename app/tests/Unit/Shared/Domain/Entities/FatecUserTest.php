d9c843b48961d24c31f0c20636c107c49ef609ee
Wesley0010012 <wglaurindo33@gmail.com>
2024-12-03 19:31:16 -0300

    test: should return the correct username

---
+++ app/tests/Unit/Shared/Domain/Entities/FatecUserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Shared\Domain\Entities;
+
+use Faker\Factory as Faker;
+
+use App\Shared\Domain\Entities\FatecUser;
+use Tests\TestCase;
+
+class FatecUserTest extends TestCase
+{
+    private readonly FatecUser $sut;
+
+    private readonly array $defaultData;
+
+    public function setUp(): void
+    {
+        $faker = Faker::create();
+
+        $this->defaultData = [
+            'username' => $faker->userName(),
+            'password' => $faker->password(),
+        ];
+
+        $this->sut = new FatecUser($this->defaultData['username'], $this->defaultData['password']);
+    }
+
+    public function testShouldReturnTheCorrectUsername(): void
+    {
+        $this->assertEquals($this->sut->getUsername(), $this->defaultData['username']);
+    }
+}
