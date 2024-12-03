f1b1c208fe0572c05088f2affca2e6d54cdc9433
Wesley0010012 <wglaurindo33@gmail.com>
2024-12-03 19:47:07 -0300

    test: should return the fullName

---
+++ app/tests/Unit/Shared/Domain/Entities/UserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Shared\Domain\Entities;
+
+use App\Shared\Domain\Entities\FatecUser;
+use Faker\Factory as Faker;
+
+use App\Shared\Domain\Entities\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    private readonly User $sut;
+
+    private readonly array $defaultData;
+
+    public function setUp(): void
+    {
+        $faker = Faker::create();
+
+        $this->defaultData = [
+            'fullName' => $faker->name(),
+            'email' => $faker->email(),
+            'password' => $faker->password(),
+            'fatecUser' => new FatecUser($faker->userName(), $faker->password()),
+            'apiToken' => $faker->regexify('[A-Za-z0-9]{60}')
+        ];
+
+        $this->sut = new User(
+            $this->defaultData['fullName'],
+            $this->defaultData['email'],
+            $this->defaultData['password'],
+            $this->defaultData['fatecUser'],
+            $this->defaultData['apiToken']
+        );
+    }
+
+    public function testShouldReturnTheFullName(): void
+    {
+        $this->assertEquals($this->sut->getFullName(), $this->defaultData['fullName']);
+    }
+}
