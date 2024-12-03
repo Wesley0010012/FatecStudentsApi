26fdd9b094932b88f5b5309e3e5abeee24b478f3
Wesley0010012 <wglaurindo33@gmail.com>
2024-12-03 19:47:19 -0300

    feat: should return the fullName

---
+++ app/app/Shared/Domain/Entities/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Shared\Domain\Entities;
+
+class User extends Entity
+{
+    private readonly string $fullName;
+    private readonly string $email;
+    private readonly string $password;
+    private readonly ?string $apiToken;
+    private readonly FatecUser $fatecUser;
+
+    public function __construct(string $fullName, string $email, string $password, FatecUser $fatecUser, ?string $apiToken = null, ?int $id = null)
+    {
+        parent::__construct($id);
+        $this->fullName = $fullName;
+        $this->email = $email;
+        $this->password = $password;
+        $this->apiToken = $apiToken;
+        $this->fatecUser = $fatecUser;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->fullName;
+    }
+}
