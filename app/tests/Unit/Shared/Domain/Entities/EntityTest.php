67b7337d020dc869129c9b1bb83007ab4d82a5f1
Wesley0010012 <wglaurindo33@gmail.com>
2024-12-03 19:20:04 -0300

    test: ensure return the correct id

---
+++ app/tests/Unit/Shared/Domain/Entities/EntityTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Shared\Domain\Entities;
+
+use App\Shared\Domain\Entities\Entity;
+use Tests\TestCase;
+
+class TestEntity extends Entity
+{
+    private readonly string $a;
+    private readonly int $b;
+
+    public function __construct(int $id, string $a, int $b)
+    {
+        parent::__construct($id);
+
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function getA(): string
+    {
+        return $this->a;
+    }
+
+    public function getB(): int
+    {
+        return $this->b;
+    }
+}
+
+class EntityTest extends TestCase
+{
+    private readonly TestEntity $sut;
+
+    public function setUp(): void
+    {
+        $this->sut = new TestEntity(1, 'a', 10);
+    }
+
+    public function testShouldReturnACorrectIdOnSuccess()
+    {
+        $this->assertEquals($this->sut->getId(), 1);
+    }
+}
