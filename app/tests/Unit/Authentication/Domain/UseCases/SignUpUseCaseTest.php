91d54ae8c50aeeeb333991b6a4b74601d97160dd
Wesley0010012 <wglaurindo33@gmail.com>
2024-12-04 21:26:15 -0300

    test: should throw if VerifyUserByEmail throws

---
+++ app/tests/Unit/Authentication/Domain/UseCases/SignUpUseCaseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\Authentication\Domain\UseCases;
+
+use Faker\Factory as Faker;
+
+use App\Authentication\Domain\Dto\SignUpInputDto;
+use App\Authentication\Domain\UseCases\SignUpUseCase;
+use App\Shared\Domain\Gateways\VerifyUserByEmail;
+use Exception;
+use Tests\TestCase;
+
+class SignUpUseCaseTest extends TestCase
+{
+    private readonly SignUpUseCase $sut;
+
+    private readonly VerifyUserByEmail $verifyUserByEmail;
+
+    private function mockInput(): SignUpInputDto
+    {
+        $faker = Faker::create();
+
+        $data = [
+            "fullName" => $faker->name(),
+            "email" => $faker->email(),
+            "password" => $faker->password(),
+            "passwordConfirmation" => $faker->password(),
+            "fatecUser" => $faker->userName(),
+            "fatecPassword" => $faker->password()
+        ];
+
+        return new SignUpInputDto(
+            $data['fullName'],
+            $data['email'],
+            $data['password'],
+            $data['passwordConfirmation'],
+            $data['fatecUser'],
+            $data['fatecPassword']
+        );
+    }
+
+    public function setUp(): void
+    {
+        $this->verifyUserByEmail = $this->createMock(VerifyUserByEmail::class);
+
+        $this->sut = new SignUpUseCase($this->verifyUserByEmail);
+    }
+
+    public function testShouldThrowIfVerifyUserByEmailThrows(): void
+    {
+        $this->verifyUserByEmail->method('existsByEmail')
+            ->willThrowException(new Exception());
+
+        $this->expectException(Exception::class);
+
+        $this->sut->execute($this->mockInput());
+    }
+}
